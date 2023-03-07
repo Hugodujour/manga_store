e536fc3de3d10dbc7c82ca119554abd263c4d42f
Hugo <hugogreg@live.fr>
2023-03-07 16:26:19 +0100

    Ajout react router DOM

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
-import './default.scss'
+import { Route, Routes } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
 import Header from "./components";
 import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
+
+import './default.scss'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Homepage />
-      </div>
+      <Routes>
+        <Route exact path="/" element={<MainLayout />}>
+        <Route path="" element={<Homepage />} />
+        <Route path="/registration" element={<Registration />} />
+        </Route >
+      </Routes>
     </div>
   );
 }
